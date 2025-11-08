7269a79ae473a8a3260b8db5cd5f0c51b6932fbb
Shinya Ihara <129726604+Shinya-GitHub-Center@users.noreply.github.com>
2025-11-08 21:08:41 +0900

    pagination added

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import select
+from app import models
+from flask import request
+from flask_paginate import Pagination, get_page_parameter
 
 app = Flask(__name__)
 app.config.from_pyfile("settings.py")
@@ -17,8 +21,18 @@ Migrate(app, db, directory=mgdir)
 
 @app.route("/")
 def index():
-    name = "index"
-    return render_template("index.html", title="Blog App", name=name)
+    stmt = select(models.Blogpost).order_by(models.Blogpost.id.desc())
+    entries = db.session.execute(stmt).scalars().all()
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    res = entries[(page - 1) * 3 : page * 3]
+    pagination = Pagination(page=page, total=len(entries), per_page=3)
+    return render_template("index.html", rows=res, pagination=pagination)
+
+
+@app.route("/entries/<int:id>")
+def show_entry(id):
+    entry = db.session.get(models.Blogpost, id)
+    return render_template("post_show.html", entry=entry)
 
 
 from app.crud.views import crud
